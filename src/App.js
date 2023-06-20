9af80b033594c437888fe3ae30ef85fbcbe860d3
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-20 21:06:15 +0530

    chore : added routes and header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import { Header } from "./components/Header/Header";
+import { Landing } from "./pages/Landing/Landing";
+import { Authentication } from "./pages/Authentication/Authentication"; 
+import { Bookmarks } from "./pages/Bookmarks/Bookmarks";
+import { Profile } from "./pages/Profile/Profile";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Header />
       </header>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/explore" element={<Landing />} />
+          <Route path="/signup" element={<Authentication/>}/>
+          <Route path="/login" element={<Authentication/>}/>
+          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route path="/profile" element={<Profile />} />
+          {/* <Route path="*" element={<Error />} /> */}
+        </Routes>
+      </div>
     </div>
   );
 }
