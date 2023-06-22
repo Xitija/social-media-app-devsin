94e4123ca0588a05bc74c9c96305624641bce2c0
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-23 02:40:05 +0530

    refactor : made Landing page dynamic

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,14 +4,26 @@ import { Navigation } from "../../components/Navigation/Navigation";
 import { HomePage } from "../../components/HomePage/HomePage";
 import { Explore } from "../../components/Explore/Explore";
 import { UserSuggestions } from "../../components/UserSuggestions/UserSuggestions";
+import { Bookmarks } from "../../components/Bookmarks/Bookmarks";
 
 export const Landing = () => {
   const location = useLocation();
 
+  const getViewComponent = () => {
+    if(location.pathname === "/")
+      return <HomePage />;
+    if(location.pathname === "/explore")
+      return <Explore />
+    if(location.pathname === "/bookmarks")
+      return <Bookmarks/>
+  }
+  
   return (
     <div style={{ display: "flex" }}>
-      <Navigation />
-      <div style={{borderRight : "0.5px solid"}}>{location.pathname === "/" ? <HomePage /> : <Explore />}</div>
+      <Navigation />  
+      {/* Pos fixed , sticky */}
+      <div style={{borderRight : "0.5px solid"}}>{getViewComponent()}</div>
+      {/*  overflow-y :scroll */}
       <UserSuggestions />
     </div>
   );
