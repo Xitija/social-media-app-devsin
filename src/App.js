76ef0c7cc9ab518671d15756bf60d6660fad41c1
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-23 02:28:16 +0530

    chore : added Auth Routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Routes, Route } from "react-router-dom";
 
 import { Header } from "./components/Header/Header";
 import { Landing } from "./pages/Landing/Landing";
-import { Authentication } from "./pages/Authentication/Authentication"; 
-import { Bookmarks } from "./pages/Bookmarks/Bookmarks";
+import { Authentication } from "./pages/Authentication/Authentication";
 import { Profile } from "./pages/Profile/Profile";
+import { RequiresAuth } from "./components/Auth/RequiresAuth";
 
 import "./App.css";
 
@@ -16,12 +16,41 @@ function App() {
       </header>
       <div className="content">
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/explore" element={<Landing />} />
-          <Route path="/signup" element={<Authentication/>}/>
-          <Route path="/login" element={<Authentication/>}/>
-          <Route path="/bookmarks" element={<Bookmarks />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/"
+            element={
+              <RequiresAuth>
+                <Landing />
+              </RequiresAuth>
+            }
+          />
+          {/* TODO: explore can be public */}
+          <Route
+            path="/explore"
+            element={
+              <RequiresAuth>
+                <Landing />
+              </RequiresAuth>
+            }
+          />
+          <Route
+            path="/bookmarks"
+            element={
+              <RequiresAuth>
+                <Landing />
+              </RequiresAuth>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <RequiresAuth>
+                <Profile />
+              </RequiresAuth>
+            }
+          />
+          <Route path="/signup" element={<Authentication />} />
+          <Route path="/login" element={<Authentication />} />
           {/* <Route path="*" element={<Error />} /> */}
         </Routes>
       </div>
