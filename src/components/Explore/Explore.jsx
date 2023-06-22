3570796e12c6ab03470a5de28e49033a8ffe967f
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-23 02:29:59 +0530

    chore : added content on Explore page

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,6 +1,20 @@
-export const Explore = () => {
+import { useData } from "../../contexts/DataContext";
 
-    return (
-        <div>Explore</div>
-    )
-}
+export const Explore = () => {
+  const { posts } = useData();
+
+  return (
+    <div>
+      <p>Explore</p>
+      {posts.map((post) => (
+        <div style={{ border: "0.5px solid" }}>
+          <p>
+            {post.name} @{post.handle} {post.createdAt}
+          </p>
+          <p>{post.content}</p>
+          <p>{post.likes.likeCount}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
