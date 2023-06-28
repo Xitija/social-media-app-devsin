c9d13c6a31a5ea9372c2916b7869886718a639ae
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-06-28 20:58:24 +0530

    chore : added Post creation and post list

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,26 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import { useData } from "../../contexts/DataContext";
+
+import { Post } from "../Post/Post";
+import { PostCard } from "../PostCard/PostCard";
 
 export const HomePage = () => {
-  const getData = async () => {
-    const productResponse = await fetch("/api/posts");
-    console.log(await productResponse.json());
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  return <div>HomePage</div>;
+  const [showPosts, setShowPosts] = useState("Latest");
+  const { getPosts } = useData();
+  
+  return (
+    <div>
+      <div>
+        <Post />
+      </div>
+      <div>
+        <button onClick={() => setShowPosts("Latest")}>Latest</button>
+        <button onClick={() => setShowPosts("Trending")}>Trending</button>
+        <p>{showPosts}</p>
+        {getPosts(showPosts).map((post) => (
+          <PostCard key={post._id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 };
