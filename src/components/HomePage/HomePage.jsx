d141ebe975ac0bc37a40315085e2fc654de2149f
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-02 17:44:34 +0530

    refctor : changes to show posts

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,19 +4,39 @@ import { useData } from "../../contexts/DataContext";
 import { Post } from "../Post/Post";
 import { PostCard } from "../PostCard/PostCard";
 
+import "./HomePage.css";
+
 export const HomePage = () => {
   const [showPosts, setShowPosts] = useState("Latest");
   const { getPosts } = useData();
-  
+
   return (
-    <div>
+    <div className="home-container">
+      <h4>Home</h4>
+      <hr />
       <div>
         <Post />
       </div>
+      <hr />
       <div>
-        <button onClick={() => setShowPosts("Latest")}>Latest</button>
-        <button onClick={() => setShowPosts("Trending")}>Trending</button>
-        <p>{showPosts}</p>
+        <div className="feed-heading">
+          <div
+            className="feed-type"
+            style={{ color: showPosts === "Latest" ? "#faba14" : "" }}
+            onClick={() => setShowPosts("Latest")}
+          >
+            Latest
+          </div>
+          <div className="vl"></div>
+          <div
+            className="feed-type"
+            style={{ color: showPosts === "Trending" ? "#faba14" : "" }}
+            onClick={() => setShowPosts("Trending")}
+          >
+            Trending
+          </div>
+        </div>
+        <hr />
         {getPosts(showPosts).map((post) => (
           <PostCard key={post._id} post={post} />
         ))}
