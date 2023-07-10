1ee6b9258dfa0262e4932067317599570d863a5f
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-10 14:06:07 +0530

    chore : added route for post detail

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,24 +8,24 @@ import { Bookmarks } from "../../components/Bookmarks/Bookmarks";
 import { Profile } from "../Profile/Profile";
 import { PostDetail } from "../../components/PostDetail/PostDetail";
 
+import "./Landing.css";
+
 export const Landing = () => {
   const location = useLocation();
 
-  console.log(location.pathname);
   const getViewComponent = () => {
     if (location.pathname === "/") return <HomePage />;
     if (location.pathname === "/explore") return <Explore />;
     if (location.pathname === "/bookmarks") return <Bookmarks />;
     if (location.pathname.toLowerCase().includes("profile")) return <Profile />;
-    if (location.pathname.toLowerCase().includes("post-details")) return <PostDetail />;
+    if (location.pathname.toLowerCase().includes("post-details"))
+      return <PostDetail />;
   };
 
   return (
-    <div style={{ display: "flex", width: "100%" }}>
+    <div style={{ display: "flex", width: "100%", height: "100%" }}>
       <Navigation />
-      {/* Pos fixed , sticky */}
-      <div style={{ width: "50%" }}>{getViewComponent()}</div>
-      {/*  overflow-y :scroll */}
+      <div className="center-section">{getViewComponent()}</div>
       <UserSuggestions />
     </div>
   );
